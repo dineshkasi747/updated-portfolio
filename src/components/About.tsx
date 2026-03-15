e23cd0bdcca4ab43e10f6b6ed057b67fee9d4241
dineshkasi747 <dineshkasi000@gmail.com>
2026-03-15 22:21:46 +0530

    initial portfolio upload

---
+++ src/components/About.tsx
@@ -0,0 +1,20 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          Experienced in building production-ready applications using modern 
+          JavaScript frameworks and scalable backend systems, with strong skills
+          in system design and performance optimization.Quick learner who writes
+          clean, maintainable code and delivers  user-centric solutions from 
+          concept to deployment.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.tsx
@@ -0,0 +1,62 @@
+import { MdArrowOutward, MdCopyright } from "react-icons/md";
+import "./styles/Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contact-section section-container" id="contact">
+      <div className="contact-container">
+        <h3>Contact</h3>
+        <div className="contact-flex">
+          <div className="contact-box">
+            <h4>Email</h4>
+            <p>
+              <a href="mailto:dineshkasi000@gmail.com" data-cursor="disable">
+                dineshkasi000@gmail.com
+              </a>
+            </p>
+            <h4>Education</h4>
+            <p>BTech in Computer Science</p>
+          </div>
+          <div className="contact-box">
+            <h4>Social</h4>
+            <a
+              href="https://github.com/dineshkasi747"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Github <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/dinesh-kasi-542233313/"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Linkedin <MdArrowOutward />
+            </a>
+            <a
+              href="https://x.com/dinesh_kas55288"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Twitter <MdArrowOutward />
+            </a>
+
+          </div>
+          <div className="contact-box">
+            <h2>
+              Designed and Developed <br /> by <span>Kasi Dinesh</span>
+            </h2>
+            <h5>
+              <MdCopyright /> 2025
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
